16c577c7fad046d39d161beddbca19272717b8ef
Imonisa Brian <mcbriandavids43@gmail.com>
2022-09-28 15:23:18 +0100

    Changd React Icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
 	const date = new Date().getFullYear();
 	return (
-		<div className='bg-gray-800 text-center py-6'>
+		<div className='bg-gray-800  text-center py-6'>
 			<p className='text-white text-sm'> CopyRight &copy; {date} </p>
 		</div>
 	);
